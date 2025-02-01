ab85c34dfca39b901eadfe26a96d18742e8635a9
alberto.gruning.zen <alberto.gruning.zen@gmail.com>
2025-02-01 00:15:42 +0000

    feat: import services to inicialize the app in in full screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   runApp(const MyApp());
 }
 
